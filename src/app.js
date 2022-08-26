c34b5ba8a4a8d9450b0b54f7a3f399dd72a07550
TexHaPb <texhapb@bk.ru>
2022-08-26 21:38:15 +0300

    added base functions of context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
-import './styles.css'
+import './styles.css';
+import { ContextMenu } from './menu';
+import { BackgroundModule } from './modules/background.module';
+import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
 
+const background = new BackgroundModule('back', 'Change back');
+const clicks = new ClicksModule('click', 'Click');
+const shape = new ShapeModule('shape', 'Shape');
+
+const contextMenu = new ContextMenu('#menu');
+
+contextMenu.add(background);
+contextMenu.add(clicks);
+contextMenu.add(shape);
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,95 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
+import { Module } from './core/module';
 
 export class ContextMenu extends Menu {
+	#moduleList // список модулей, подключенных к контекстному меню
+	#contextMenuContainer // селектор контейнера, при клике на который вызывается контекстное меню
 
+	constructor(selector, containerSelector) {
+		super(selector);
+
+		this.containerSelector = containerSelector || 'body'; // по умолчанию считаем что клик будет по всему документу
+
+		this.#contextMenuContainer = document.querySelector(this.containerSelector);
+		this.#moduleList = [];
+
+		this.#render(); // создаем элементы контекстного меню
+		this.#setup(); // подключаем к неему обработчики
+	}
+
+	// отрисовка контекстного меню
+	#render() {
+		this.el.classList.add('menu');
+		this.el.innerHTML = '';
+
+		this.#moduleList.forEach(module => {
+			this.el.insertAdjacentHTML('beforeend', module.toHTML()); // добавляемм в меню каждый подключенный модуль
+		});
+	}
+
+	// установка обработчиков для контекстного меню
+	#setup() {
+
+		this.contextOpenHandler = this.contextOpenHandler.bind(this); // привязываем контекст к обработчику событий
+		this.#contextMenuContainer.addEventListener('contextmenu', this.contextOpenHandler);
+
+		this.contextClickHandler = this.contextClickHandler.bind(this); // привязываем контекст к обработчику событий
+		this.el.addEventListener('click', this.contextClickHandler);
+	}
+
+	// обработчик вызова контекстного меню
+	contextOpenHandler(event) {
+		// ничего не делаем, если к контекстному меню не привязано ни одного модуля
+		if (!this.#moduleList.length) {
+			return false;
+		}
+
+		event.preventDefault();
+
+		// позиционируем меню
+		this.el.style.top = `${event.clientY}px`;
+		this.el.style.left = `${event.clientX}px`;
+
+		this.open();
+	}
+
+	// обработчик нажатия на пункты меню
+	contextClickHandler(event) {
+		const { target } = event;
+		if (target.dataset?.type) {
+			const mod = this.#moduleList.find(module => module.type === target.dataset.type)
+			mod?.trigger();
+		}
+		this.close();
+	}
+
+	// открытие контекстного меню
+	open() {
+		this.el.classList.add('open');
+	}
+
+	// закрытие контекстного меню
+	close() {
+		this.el.classList.remove('open');
+	}
+
+	// добавление модуля к контекстному меню
+	add(module) {
+		// проверяем что модуль передан и относится к классу Module
+		if (!module || !module instanceof Module) {
+			return;
+		}
+
+		this.#moduleList.push(module);
+		this.#render(); // перерисовываем контекстное меню
+	}
+
+	// удаление
+	destroy() {
+		this.el.classList.remove('menu', 'open');
+
+		// удаляем прикрепленные обработики событий
+		this.#contextMenuContainer.removeEventListener('contextmenu', this.contextOpenHandler);
+		this.el.removeEventListener('click', this.contextClickHandler);
+	}
 }
