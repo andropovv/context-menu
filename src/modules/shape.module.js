5f006cfa4ce283cdceb72f168bb4d55bb18abd5d
andropovv <5andry8831@gmail.com>
2022-08-27 19:20:43 +0300

    модуль с фигурами

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,7 +1,83 @@
-import { Module } from '../core/module'
+import { Module } from "../core/module";
+import { random } from "../utils";
+import "./shape.css";
 
 export class ShapeModule extends Module {
-	trigger() {
-		console.log('shape');
-	}
+  constructor(type, text) {
+    super(type, text);
+    if (!type) {
+      throw new Error('Please specify "type" param');
+    }
+    if (!text) {
+      throw new Error('Please specify "text" param');
+    }
+    this.type = type;
+    this.text = text;
+    this.size = [0, 0];
+    this.figures = ["square", "circle", "heart"];
+  }
+
+  trigger() {
+    const figure = this.createFigure();
+
+    document.body.append(figure);
+
+    this.addTransform = this.addTransform.bind(this);
+    this.removeTransform = this.removeTransform.bind(this);
+
+    figure.addEventListener("mouseover", (e) => this.addTransform(e.target));
+    figure.addEventListener("mouseleave", (e) =>
+      this.removeTransform(e.target)
+    );
+
+    setTimeout(() => {
+      this.removeFigure(figure);
+    }, 5000);
+  }
+
+  addTransform(element) {
+    element.style.transform = `rotate(${random(90, 360)}deg)`;
+    element.style.transition = `all ease 5s`;
+  }
+
+  removeTransform(element) {
+    element.style.transform = `none`;
+  }
+
+  createFigure() {
+    const figure = document.createElement("div");
+    figure.classList.add(this.getRandomClass());
+    figure.style.transform = `scale(${random(2, 10) / 10})`;
+    figure.style.borderColor = this.getRandomColor();
+
+    figure.style.position = "absolute";
+    figure.style.left = `${this.getRandomPosition()[1]}px`;
+    figure.style.top = `${this.getRandomPosition()[0]}px`;
+
+    return figure;
+  }
+
+  getRandomClass() {
+    return this.figures[random(0, this.figures.length - 1)];
+  }
+
+  getRandomPosition() {
+    return [
+      random(200, window.innerHeight - 200),
+      random(200, window.innerWidth - 200),
+    ];
+  }
+
+  getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  removeFigure(figure) {
+    document.body.removeChild(figure);
+  }
 }
