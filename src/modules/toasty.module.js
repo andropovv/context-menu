262836142a529352daa87e526936cdbcaa44bb6c
TexHaPb <texhapb@bk.ru>
2022-08-28 13:52:09 +0300

    realized functions

--- src/modules/toasty.module.js
+++ src/modules/toasty.module.js
@@ -1,7 +1,48 @@
 import { Module } from '../core/module'
+import * as Utils from '../utils'
+import './toasty.css'
+import Photo from '../assets/img/iou.png'
+import ToastySound from '../assets/audio/toasty.mp3'
 
 export class ToastyModule extends Module {
+	#isRunning
+
+	#toastyContainer
+
+	constructor(type, text) {
+		super(type, text);
+
+		this.#isRunning = false;
+
+	}
+
 	trigger() {
-		console.log('toasty');
+		this.#create();
+		Utils.playAudio(ToastySound);
+
+		setTimeout(() => {
+			this.destroy();
+		}, 2000);
+	}
+
+	#create() {
+		this.#toastyContainer = document.createElement('div');
+		this.#toastyContainer.classList.add('toasty');
+
+		const img = document.createElement('img');
+		img.src = Photo;
+		img.classList.add('toasty__img');
+
+		const text = document.createElement('div');
+		text.classList.add('toasty__text');
+		text.textContent = 'Toasty!!!';
+
+		this.#toastyContainer.append(img, text);
+
+		document.body.append(this.#toastyContainer);
+	}
+
+	destroy() {
+		this.#toastyContainer?.remove();
 	}
 }
