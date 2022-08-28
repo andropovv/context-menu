39ca3a51f5dcf1d74f8439b20878e8ecbf50a437
andropovv <5andry8831@gmail.com>
2022-08-28 16:37:29 +0300

    sounds module

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
-import './styles.css';
-import { ContextMenu } from './menu';
-import { BackgroundModule } from './modules/background.module';
-import { ClicksModule } from './modules/clicks.module';
-import { ShapeModule } from './modules/shape.module';
-import { TimerModule } from './modules/timer.module';
-import { ToastyModule } from './modules/toasty.module';
+import "./styles.css";
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { ShapeModule } from "./modules/shape.module";
+import { TimerModule } from "./modules/timer.module";
+import { ToastyModule } from "./modules/toasty.module";
+import { RandomSound } from "./modules/randomsound.module";
 
-const background = new BackgroundModule('back', 'Изменить фон');
-const clicks = new ClicksModule('click', 'Анализ кликов');
-const shape = new ShapeModule('shape', 'Создать фигуру');
-const timer = new TimerModule('timer', 'Установить таймер');
-const toasty = new ToastyModule('toasty', "Toasty!!!");
+const background = new BackgroundModule("back", "Изменить фон");
+const clicks = new ClicksModule("click", "Анализ кликов");
+const shape = new ShapeModule("shape", "Создать фигуру");
+const timer = new TimerModule("timer", "Установить таймер");
+const toasty = new ToastyModule("toasty", "Toasty!!!");
+const randomSound = new RandomSound("sound", "Случайный звук");
 
-const contextMenu = new ContextMenu('#menu');
+const contextMenu = new ContextMenu("#menu");
 
 contextMenu.add(background);
 contextMenu.add(clicks);
 contextMenu.add(shape);
 contextMenu.add(timer);
 contextMenu.add(toasty);
+contextMenu.add(randomSound);
