25a8b825398af4fd3876d9994cf7ed1ca1225c31
andropovv <5andry8831@gmail.com>
2022-08-28 18:32:43 +0300

    изменено название модуля звуков поправлен код фигур

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ import { ClicksModule } from "./modules/clicks.module";
 import { ShapeModule } from "./modules/shape.module";
 import { TimerModule } from "./modules/timer.module";
 import { ToastyModule } from "./modules/toasty.module";
-import { RandomSound } from "./modules/randomsound.module";
+import { RandomSoundModule } from "./modules/randomsound.module";
 
 const background = new BackgroundModule("back", "Изменить фон");
 const clicks = new ClicksModule("click", "Анализ кликов");
 const shape = new ShapeModule("shape", "Создать фигуру");
 const timer = new TimerModule("timer", "Установить таймер");
 const toasty = new ToastyModule("toasty", "Toasty!!!");
-const randomSound = new RandomSound("sound", "Случайный звук");
+const randomSound = new RandomSoundModule("sound", "Случайный звук");
 
 const contextMenu = new ContextMenu("#menu");
 
