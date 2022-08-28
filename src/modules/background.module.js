764207c584b2c9327ea19b5de27b9c2120461c41
wallnnut <81454917+wallnnut@users.noreply.github.com>
2022-08-28 16:10:45 +0300

    inited getPhoto func

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,7 +1,29 @@
-import { Module } from '../core/module'
-
+import { Module } from "../core/module";
+import { random } from "../utils";
+import { getPhoto } from "../utils";
+import "./background.css";
 export class BackgroundModule extends Module {
-	trigger() {
-		console.log('background');
-	}
+  #url;
+  #photoArray;
+  #body;
+  constructor(type, text) {
+    super(type, text);
+    this.#body = document.body;
+    this.#url =
+      "https://api.unsplash.com/photos/?client_id=hyjD8yAQ-fkXg7qz8w6vJRpqFHOTzk-957CMFtN9b6g";
+    this.#photoArray = [];
+  }
+  trigger() {
+    this.pushToArray(getPhoto(this.#url));
+    this.#body.style.backgroundImage = `url(${
+      this.#photoArray[random(0, this.#photoArray.length)]
+    })`;
+  }
+  pushToArray(callback) {
+    callback.then((photo) => {
+      photo.forEach((element) => {
+        this.#photoArray.push(element.urls.full);
+      });
+    });
+  }
 }
